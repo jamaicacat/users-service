ec9b50feddb8033e552d2c7d397f83a226a5ac2e
Nikita Klochko <58182478+jamaicacat@users.noreply.github.com>
2021-08-20 10:26:03 +0300

    remove builded dist directory

--- dist/dto/create-user-dto.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-export declare class CreateUserDto {
-    firstName: string;
-    lastName: string;
-    email: string;
-    photo: Buffer;
-}
--- dist/dto/user-id.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class UserIdDto {
-    id: string;
-}
